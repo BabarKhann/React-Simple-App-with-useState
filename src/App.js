22ac613c4ab3513304796f4919ee1ccff7b68f3a
Babar Khan <babarkhan138@gmail.com>
2020-03-08 03:28:06 +0500

    Simple App with useState and Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Tweet from './Tweet';
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('Hiii');
+
+  const [users, setusers] = useState([
+    { name: 'John', message: 'im john snow', likes: 55 },
+    { name: 'Brad', message: 'im Brad Pitt', likes: 55 },
+    { name: 'Keanu', message: 'im Keanu Reeves', likes: 55 },
+  ])
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
+
+  const changeName = () => {
+    setName("i'm changed");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} likes={user.likes} />
+      ))}
+      <br />
+      <hr />
+      <button onClick={increment}>Increment</button>
+      <h2 className={isRed ? 'red' : ''}>{count}</h2>
+      <h2>{name}</h2>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
